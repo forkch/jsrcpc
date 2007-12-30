dfc70317e526690a5fdbe246a84fdbbe39406fd9
andre_schenk <andre_schenk@34691b4d-6891-4d8c-824a-cadf0a7411ec>
2007-12-30 14:00:01 +0000

    removed log from source file because of unmappable German umlauts

--- jsrcpc/src/de/dermoba/srcp/common/exception/SRCPException.java
+++ jsrcpc/src/de/dermoba/srcp/common/exception/SRCPException.java
@@ -1,33 +1,9 @@
-/*
- * $RCSfile: SRCPException.java,v $
- *
- $Log: not supported by cvs2svn $
- Revision 1.3  2006/01/02 15:55:54  fork_ch
- - Exception generation is done by ReceivedException Handler
- - added many exceptions
- - added a "old protocol"-mode
-
- Revision 1.3  2005/08/08 08:02:36  harders
- Stand nach Urlaub
-
- Revision 1.2  2005/07/09 13:11:58  harders
- balkon050709
-
- Revision 1.1  2005/06/30 14:41:31  harders
- Aufger�umte erste Version
-
- Revision 1.1.1.1  2002/01/08 18:21:54  osc3
- import of jsrcpd
-
-
- */
-
 package de.dermoba.srcp.common.exception;
 
 /**
  *
  * @author  osc
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 
 public abstract class SRCPException extends java.lang.Exception {
